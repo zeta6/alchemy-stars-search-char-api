4c86ddd13f6c82d858f23698563d56ddbe70f774
Kwon Ohjin <lazy1344@naver.com>
2021-07-26 01:41:11 +0900

    add logo field

--- aurorian_searcher_api/character/models/character.py
+++ aurorian_searcher_api/character/models/character.py
@@ -7,21 +7,20 @@ from .faction import Faction
 from .personality import Personality
 
 def upload_path(instance, filename):
-    return '{0}/icon/{1}'.format(instance.name_alphabet, filename)
+    return '{0}/icon_logo/{1}'.format(instance.name_alphabet, filename)
 
 class Character(models.Model):
     name = models.CharField(max_length=50)
     name_alphabet = models.CharField(max_length=30)
-    # state complete
     icon = models.ImageField(upload_to=upload_path, height_field=None, width_field=None, max_length=None, null=True, blank=True) 
-    # image complete
+    logo = models.ImageField(upload_to=upload_path, height_field=None, width_field=None, max_length=None, null=True, blank=True) 
     rarity = models.PositiveSmallIntegerField(blank=True, null=True)
     main_attribute = models.ForeignKey("MainAttribute", verbose_name="주속성", on_delete=models.SET_NULL, null=True, blank=True)
     sub_attribute = models.ForeignKey("SubAttribute", verbose_name="부속성", on_delete=models.SET_NULL, null=True, blank=True)
     char_class = models.ForeignKey("CharClass", verbose_name="클래스", on_delete=models.SET_NULL, null=True, blank=True)
     faction = models.ForeignKey("Faction", verbose_name="소속 세력", on_delete=models.SET_NULL, null=True, blank=True)
     personality = models.ForeignKey("Personality", verbose_name="성격", on_delete=models.SET_NULL, null=True, blank=True)
-
+    #state, image, charin_skill, active_skill, equip_skill, equipment, breakthrough, ascension, voice 
     class Meta:
         verbose_name = "character"
         verbose_name_plural = "characters"
