b47efdce052b2a777eee7cb5e16e9c3bcd5a2a5f
Kwon Ohjin <lazy1344@naver.com>
2021-07-28 05:32:14 +0900

    -

--- aurorian_searcher_api/aurorian_searcher_api/settings.py
+++ aurorian_searcher_api/aurorian_searcher_api/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -150,7 +150,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    STATIC_ROOT, MEDIA_ROOT
+    MEDIA_ROOT, 'static'
 ]
 
 #corsheaders
