19519b08a671253c4e27e7032fc183b7599bf2df
Kwon Ohjin <lazy1344@naver.com>
2021-07-28 05:16:36 +0900

    fix statc urlpattenrs

--- aurorian_searcher_api/aurorian_searcher_api/urls.py
+++ aurorian_searcher_api/aurorian_searcher_api/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('api/', include(router.urls))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
