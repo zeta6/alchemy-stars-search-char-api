652b8fc1ec2def261580892aa35cd6265f4fd7e5
Kwon Ohjin <lazy1344@naver.com>
2021-07-28 01:20:02 +0900

    add allowed host aws address

--- aurorian_searcher_api/aurorian_searcher_api/settings.py
+++ aurorian_searcher_api/aurorian_searcher_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.124.59.83"]
 
 
 # Application definition
