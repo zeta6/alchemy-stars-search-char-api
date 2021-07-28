d52177e9c5ff95925d1ebb5b0f469916e16f48fc
Kwon Ohjin <lazy1344@naver.com>
2021-07-28 09:51:48 +0900

    awteat

--- aurorian_searcher_api/aurorian_searcher_api/settings.py
+++ aurorian_searcher_api/aurorian_searcher_api/settings.py
@@ -159,4 +159,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:3000',
     'http://13.124.59.83:3000',
     'http://13.124.59.83:80',
+    'http://13.124.59.83',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
