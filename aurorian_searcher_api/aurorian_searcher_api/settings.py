8911463c397c97b3c5098ef9856b31e5c48c38ec
Kwon Ohjin <lazy1344@naver.com>
2021-07-28 09:36:35 +0900

    test

--- aurorian_searcher_api/aurorian_searcher_api/settings.py
+++ aurorian_searcher_api/aurorian_searcher_api/settings.py
@@ -156,5 +156,6 @@ STATICFILES_DIRS = [
 #corsheaders
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://13.124.59.83:3000',
 ]
