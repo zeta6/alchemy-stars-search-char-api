928862ec8b703366f2a92e8f6ec7e85424b7461c
Kwon Ohjin <lazy1344@naver.com>
2021-07-29 11:57:17 +0900

    gwagewa

--- aurorian_searcher_api/aurorian_searcher_api/settings.py
+++ aurorian_searcher_api/aurorian_searcher_api/settings.py
@@ -157,14 +157,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 # ]
 
 # corsheaders
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://54.180.126.198:3000',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+#     'http://54.180.126.198:3000',
+# ]
 
 
 
