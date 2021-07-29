f1eea1509b694d02a02cc902c646e48eaa0a824e
Kwon Ohjin <lazy1344@naver.com>
2021-07-29 12:01:37 +0900

    rqwrqw

--- aurorian_searcher_api/aurorian_searcher_api/settings.py
+++ aurorian_searcher_api/aurorian_searcher_api/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["54.180.126.198"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
