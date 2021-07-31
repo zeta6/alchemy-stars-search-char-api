747f2332009cfd4e76dcc187c013f1d4e9e34ca1
Kwon Ohjin <lazy1344@naver.com>
2021-07-31 14:12:43 +0900

    fix special_role mode

--- aurorian_searcher_api/character/models/special_role.py
+++ aurorian_searcher_api/character/models/special_role.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class SpecialRole(models.Model):
+     
+    sp_role_id = models.OneToOneField("Character", related_name="special_role", on_delete=models.CASCADE)
     tile_change = models.BooleanField(default=False)
     tile_reset = models.BooleanField(default=False)
     teleport = models.BooleanField(default=False)
     heal = models.BooleanField(default=False)
+    input_check = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "special role"
