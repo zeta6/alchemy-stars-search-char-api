9716cd1772f5cb453b414bb82b629e8a0a2bc929
Kwon Ohjin <lazy1344@naver.com>
2021-08-20 02:30:17 +0900

    Recruiting page under construction

--- aurorian_searcher_api/recruit/models.py
+++ aurorian_searcher_api/recruit/models.py
@@ -5,6 +5,8 @@ class RecruitBanner(models.Model):
     name = models.CharField(max_length=50,blank=True)  
     image = models.ImageField(upload_to="recruit_images", height_field=None, width_field=None, max_length=None, blank=True)  
     char_id_not_included = models.TextField(blank=True)
+    rarity_5_pickup = models.CharField(max_length=10,blank=True)
+    rarity_6_pickup = models.CharField(max_length=10,blank=True)  
 
     class Meta:
         verbose_name = "Recruit banner"
--- aurorian_searcher_api/recruit/serializers.py
+++ aurorian_searcher_api/recruit/serializers.py
@@ -5,4 +5,4 @@ from .models import RecruitBanner
 class RecruitBannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecruitBanner
-        fields = ('id', 'name', 'image', 'char_id_not_included')
+        fields = ('id', 'name', 'image', 'char_id_not_included', 'rarity_5_pickup', 'rarity_6_pickup')
